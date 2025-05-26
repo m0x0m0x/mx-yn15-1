2425963e7953c9611d1bc4dd64c4886b41ae8215
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-05-26 17:10:47 +0000

    🫦

--- WO/ta1/app/dashboard/[slug]/page.tsx
+++ WO/ta1/app/dashboard/[slug]/page.tsx
@@ -1,5 +1,8 @@
 // Dynamic route
 
-export default function DashboardRoute() {
+type params = Promise<{ slug: string }>;
+
+export default async function DashboardRoute({ params }: { params: params }) {
+  const { slug } = await params;
   return <h1> Hello from dashboard/slug/page.tsx</h1>;
 }
