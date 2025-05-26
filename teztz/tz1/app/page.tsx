dac0857bcb58a34be604bcdc57ff697f167c00ca
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-05-26 14:01:15 +0000

    🫦

--- teztz/tz1/app/page.tsx
+++ teztz/tz1/app/page.tsx
@@ -16,23 +16,6 @@ export default function Home() {
   return (
     <>
       <h1>Whatup</h1>
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          <button className=" px-4 py-2 rounded">Open Alert Dialog</button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction>Confirm</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
     </>
   );
 }
