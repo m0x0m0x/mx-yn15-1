e033fa629666e7ad8a7aaa43622e194c692ae377
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-05-26 13:51:06 +0000

    🫦

--- teztz/tz1/app/page.tsx
+++ teztz/tz1/app/page.tsx
@@ -1,5 +1,38 @@
 import Image from "next/image";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
 export default function Home() {
-  return <h1>Whatup</h1>;
+  return (
+  <h1>Whatup</h1>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded">
+          Open Alert Dialog
+        </button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Confirm</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+)
 }
