9598f7afef3ab21c96682376d1d9660ecb607ab3
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-05-26 14:19:52 +0000

    🫦

--- teztz/tz1/app/layout.tsx
+++ teztz/tz1/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex items-center justify-center min-h-screen flex-col`}
       >
