902cdf8407f3f0981c747566d504ebf06fac9118
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-05-26 13:52:47 +0000

    🫦

--- teztz/tz1/app/page.tsx
+++ teztz/tz1/app/page.tsx
@@ -14,25 +14,27 @@ import {
 
 export default function Home() {
   return (
-  <h1>Whatup</h1>
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded">
-          Open Alert Dialog
-        </button>
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Confirm</AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
-)
+    <>
+      <h1>Whatup</h1>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded">
+            Open Alert Dialog
+          </button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Confirm</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
 }
